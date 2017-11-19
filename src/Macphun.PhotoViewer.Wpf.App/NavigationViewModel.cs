0901d92f1f11597f968305aa1b6cd91b161be16b
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2017-11-19 20:21:51 +0200

    Implement main view

---
+++ src/Macphun.PhotoViewer.Wpf.App/NavigationViewModel.cs
@@ -0,0 +1,63 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2017 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Macphun.PhotoViewer.ViewModels;
+
+namespace Macphun.PhotoViewer.Wpf.App
+{
+    public sealed class NavigationViewModel : INotifyPropertyChanged
+    {
+        private readonly ThumbnailsViewModel _thumbnailsViewModel;
+        private readonly IList<Uri> _uriRepository;
+
+        private object _selectedViewModel;
+
+        public NavigationViewModel()
+        {
+            _uriRepository = new List<Uri>();
+            SelectedViewModel = _thumbnailsViewModel = new ThumbnailsViewModel(_uriRepository);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public object SelectedViewModel
+        {
+            get => _selectedViewModel;
+
+            set
+            {
+                _selectedViewModel = value;
+                
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SwitchToThumbnailViewCommand => new RelayCommand(SwitchToThumbnailView);
+
+        public ICommand SwitchToImageViewCommand => new RelayCommand(SwitchToImageView);
+
+        private void SwitchToThumbnailView()
+        {
+            SelectedViewModel = _thumbnailsViewModel;
+        }
+
+        private void SwitchToImageView()
+        {
+            // TODO: Implement
+            throw new NotImplementedException();
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
