a2a8f8da99b70377d19ca04f5e46546a22cbd07b
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2017-11-19 20:21:36 +0200

    Implement Thumbnails

---
+++ src/Macphun.PhotoViewer.ViewModels/ThumbnailsViewModel.cs
@@ -0,0 +1,59 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2017 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Macphun.PhotoViewer.Wpf;
+
+namespace Macphun.PhotoViewer.ViewModels
+{
+    public sealed class ThumbnailsViewModel
+    {
+        private readonly IList<Uri> _uriRepository;
+
+        public ThumbnailsViewModel(IList<Uri> uriRepository)
+        {
+            if (uriRepository == null)
+            {
+                throw new ArgumentNullException(nameof(uriRepository));
+            }
+
+            _uriRepository = uriRepository;
+
+            Thumbnails = new ObservableCollection<ThumbnailViewModel>();
+        }
+
+        public ObservableCollection<ThumbnailViewModel> Thumbnails { get; }
+
+        public ICommand AddThumbnailsCommand => new DelegateCommand<IEnumerable<string>>(AddThumbnailsAsync);
+
+        private IEnumerable<ThumbnailViewModel> AddThumbnails(IEnumerable<string> paths)
+        {
+            var uris = paths.Select(x => new Uri(x)).ToList();
+
+            foreach (var uri in uris)
+            {
+                _uriRepository.Add(uri);
+            }
+
+            return uris.Select(x => new ThumbnailViewModel(x));
+        }
+
+        private async void AddThumbnailsAsync(IEnumerable<string> paths)
+        {
+            var thumbnails = await Task.Factory.StartNew(() => AddThumbnails(paths));
+
+            foreach (var thumbnail in thumbnails)
+            {
+                Thumbnails.Add(thumbnail);
+            }
+        }
+    }
+}
---
+++ src/Macphun.PhotoViewer.Views/ThumbnailView.xaml.cs
@@ -0,0 +1,18 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2017 by Shogun, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System.Windows.Controls;
+
+namespace Macphun.PhotoViewer.Views
+{
+    public partial class ThumbnailView : UserControl
+    {
+        public ThumbnailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
